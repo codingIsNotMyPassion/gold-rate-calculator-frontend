dfd723658ff4c9106ffb9a11063a4eda85a76306
codingIsNotMyPassion <venkatakrishnakoneti@gmail.com>
2022-06-30 21:34:31 +0530

    css changes

--- src/components/home page/Home.jsx
+++ src/components/home page/Home.jsx
@@ -3,25 +3,24 @@ import "./Home.css"
 import api from '../api/api'
 
 const Home = () => {
-    const [price, setPrice] = useState()
-    const [Purity, setPurity] = useState("")
+    const [price, setPrice] = useState(null)
+    const [Purity, setPurity] = useState("24K")
     const [weight, setWeight] = useState(0)
     const [total, setTotal] = useState(0)
 
     //get gold prices from API
-    useEffect(()=>{
+    useEffect(() => {
         const getGoldPrices = async () => {
             try {
-              const response = await api.get("/prices")
-                console.log(response.data)
+                const response = await api.get("/prices")
                 setPrice(response.data)
             } catch (error) {
                 console.log(error)
             }
         }
         getGoldPrices()
-    },[])
-    
+    }, [])
+
     // handle weight
     const handleWeightChange = (e) => {
         setWeight(e.target.value)
@@ -43,22 +42,24 @@ const Home = () => {
         <>
             <div className="main-container">
                 <header>
-                <h1>Gold price calculator</h1>
+                    <h1>Gold price calculator</h1>
                 </header>
-                <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                        <label>Purity</label>
-                        <select className="form-select" value={Purity}
-                            onChange={handlePurityChange}>
-                            <option value="24K">24K</option>
-                            <option value="22K">22K</option>
-                        </select>
-                    </div>
-                    <label>weight (in grams) </label>
-                    <input type="text" value={weight} onChange={handleWeightChange} />
-                    <button className="btn btn-primary" type="submit">Calculate</button>
-                </form>
-                <p>The gold price is {total}</p>
+                <main>
+                    <form className="form-container" onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label className='form-label'>Purity</label>
+                            <select className="form-select" value={Purity}
+                                onChange={handlePurityChange}>
+                                <option value="24K">24K</option>
+                                <option value="22K">22K</option>
+                            </select>
+                        </div>
+                        <label className='form-label'>weight (in grams) </label>
+                        <input type="number" value={weight} onChange={handleWeightChange} />
+                        <button className="btn btn-primary" type="submit">Calculate</button>
+                    </form>
+                    <p>The gold price is {total}</p>
+                </main>
             </div>
         </>
     )
